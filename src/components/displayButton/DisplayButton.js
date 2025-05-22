e7a0fef6c7045b797ed03211124442161fcc026f
Renzo Santos <orrego.st.santos@gmail.com>
2025-05-22 12:29:41 -0500

    use hook usekeyup in displaybutton

--- src/components/displayButton/DisplayButton.js
+++ src/components/displayButton/DisplayButton.js
@@ -1,35 +1,22 @@
-import { useEffect } from 'react';
-import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { useKeyUp } from 'hooks';
+import { Button } from 'react-bootstrap';
 
 export function DisplayButton({ value, presenting, onToggle }) {
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.ctrlKey && event.key === 'p') {
-        event.preventDefault(); // evita que se abra la impresión
-        onToggle((prev) => !prev);
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [onToggle]);
-
+  useKeyUp('KeyP', () => onToggle((value) => !value), { ctrl: true });
   if (!presenting) {
     return null;
   }
 
   return (
-    <OverlayTrigger placement="bottom" overlay={<Tooltip>ctrl + p</Tooltip>}>
-      <Button
-        className={value && presenting ? 'mb-4 pulse' : 'mb-4'}
-        block
-        size="lg"
-        variant={value ? 'light' : 'secondary'}
-        onClick={() => onToggle((prev) => !prev)}
-        disabled={!presenting}
-      >
-        {value ? 'Proyectar' : 'Mostrar Logo'}
-      </Button>
-    </OverlayTrigger>
+    <Button
+      className={value && presenting ? 'mb-4 pulse' : 'mb-4'}
+      block
+      size="lg"
+      variant={value ? 'light' : 'secondary'}
+      onClick={() => onToggle((value) => !value)}
+      disabled={!presenting}
+    >
+      {value ? 'Proyectar' : 'Mostrar Logo'}
+    </Button>
   );
 }
